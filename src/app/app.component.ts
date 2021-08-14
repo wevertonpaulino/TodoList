11c29523380e0e911a887200083ee248eabc83f5
Weverton <wevertoncesar@gmail.com>
2021-08-14 10:28:52 -0300

    Exibe os dados na tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push('passear com o cachorro');
+    this.todos.push('lavar o carro');
+    this.todos.push('cortar o cabelo');
+  }
 }
