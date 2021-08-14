def45a522cdf2a9bcf749a1a460de8a2a47b8104
Weverton <wevertoncesar@gmail.com>
2021-08-14 11:27:30 -0300

    Carrega os itens da lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,8 @@ export class AppComponent {
         Validators.required,
       ])]
     });
+
+    this.load();
   }
 
   clear() {
@@ -31,6 +33,7 @@ export class AppComponent {
     const title = this.form.controls['title'].value;
 
     this.todos.push(new Todo(id, title, false));
+    
     this.save();
     this.clear();
   }
@@ -41,18 +44,32 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+
+    this.save();
   }
 
   save() {
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    
+    if (data) {
+      this.todos = JSON.parse(data);
+    }
   }
 }
