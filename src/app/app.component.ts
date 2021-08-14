a47af03a1641197bacac102d3c18127a12f3e9aa
Weverton <wevertoncesar@gmail.com>
2021-08-14 10:53:45 -0300

    Remove um item da lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,12 @@ export class AppComponent {
     this.todos.push(new Todo(2, 'lavar o carro', false));
     this.todos.push(new Todo(3, 'cortar o cabelo', false));
   }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
